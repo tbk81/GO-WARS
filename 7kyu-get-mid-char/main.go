e26ec61299d11d001cebd008256a07c60efc635f
tbk81 <dev@tbk81.com>
2025-02-18 08:30:13 -0800

    finsh mid char

--- 7kyu-get-mid-char/main.go
+++ 7kyu-get-mid-char/main.go
@@ -1,12 +1,56 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strings"
+)
 
 func getMidChar(s string) string {
-	return s
+	xs := strings.Split(s, "")
+	ld2 := len(xs) / 2
+	var newString string
+	if (len(xs))%2 == 0 {
+		newString = xs[ld2-1] + xs[ld2]
+	} else {
+		newString = xs[ld2]
+	}
+	return newString
 }
 
 func main() {
-	x := getMidChar("Hello")
+	i := "A"
+	x := getMidChar(i)
 	fmt.Println(x)
+	fmt.Printf("len: %v\nlen/2: %v\n", len(i), len(i)/2)
+	fmt.Println(len(i) % 2)
+
 }
+
+/*
+You are going to be given a non-empty string. Your job is to return the middle character(s) of the string.
+
+    If the string's length is odd, return the middle character.
+    If the string's length is even, return the middle 2 characters.
+
+Examples:
+
+"test" --> "es"
+"testing" --> "t"
+"middle" --> "dd"
+"A" --> "A"
+
+
+n := len(s)
+    if n==0 {return s}
+    return s[(n - 1) / 2 : n / 2 + 1]
+
+
+OR
+
+mid := len(s)/2
+  if len(s) % 2 == 1 {
+    return string(s[mid])
+  }
+  return string(s[mid-1]) + string(s[mid])
+
+*/
